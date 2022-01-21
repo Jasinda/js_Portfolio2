ca2a3a234951ee36f7e59c58a60998b35418d2b8
Jasinda <90047395+Jasinda@users.noreply.github.com>
2022-01-21 15:35:10 +0200

    Add files via upload

---
+++ Chapter 8/chap08.js
@@ -0,0 +1,11 @@
+var myCar = {
+brand:"Renault",
+Model : "RS",
+Year : "2019",
+COndition: "Sexy"
+}
+
+ document.getElementById.innerHTML= myCar.brand;
+ document.getElementById.innerHTML= myCar.model;
+ document.getElementById.innerHTML= myCar.year;
+ document.getElementById.innerHTML= myCar.condition;
---
+++ Test/test.js
@@ -0,0 +1,10 @@
+var textBox = document.getElementById("txtBox");
+var output = document.getElementById("txt");
+
+document.getElementById("myBtn").onclick = displayName;
+/* document.getElementById("myBtn").addEventListener("click", displayName); */
+
+function displayName(){
+    var input = textBox.value;
+    output.innerHTML = "My name is " + input;
+}
